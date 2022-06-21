a339dbb8b44c7a85d7ceaefb4ef73db7bb1687f3
Rhomeinel <rhomeinel@gmail.com>
2022-06-21 12:29:22 -0700

    Solve indent error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,9 @@ def safe_print_list(my_list=[], x=0):
     num = 0
 
     for i in range(0, x)
-        try:
-            print("{}".format(my_list[i]), end="")
-        except:
-            break
-            
+    try:
+        print("{}".format(my_list[i]), end="")
+    except IndexError:
+        break
     print()
     return num
