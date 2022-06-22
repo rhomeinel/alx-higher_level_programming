ec416345deb325593545ebf50684a999c5a2ddea
Rhomeinel <rhomeinel@gmail.com>
2022-06-22 00:44:02 -0700

    Fix Indentation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Define class Square with private attribute size
+"""
+
+
+class Square:
+    """Represent s squqre."""
+
+    def __init__(self, size):
+        """
+        Initializes square
+
+        Attributes:
+            size: size of a square
+        """
+        self._size = size
