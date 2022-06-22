b1a829f71a38194bc112ac062fa47a482ee27ea6
Rhomeinel <rhomeinel@gmail.com>
2022-06-22 05:17:51 -0700

    Add code to file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pyth0n3
+"""
+Module 2-square
+Defines class Square with private size and validates size
+"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """
+        Initializes square
+        Attributes:
+            size (int): size=0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = size
