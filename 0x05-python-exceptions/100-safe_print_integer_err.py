bbac94083c91db1a78953564b6f91051cbc745f9
Rhomeinel <rhomeinel@gmail.com>
2022-06-22 20:24:57 -0700

    Add code to file

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_function(fct, *args):
+def safe_print_integer_err(value):
     import sys
     try:
-        ret = fct(*args)
-        return ret
+        print("{:d}".format(value))
+        return True
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        return None
+        return False
