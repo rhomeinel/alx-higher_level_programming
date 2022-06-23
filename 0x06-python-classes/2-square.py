671b56755e6d845b378669ebd59fab8cf7ff6269
Rhomeinel <rhomeinel@gmail.com>
2022-06-22 19:44:35 -0700

    Add code to file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,21 +1,14 @@
-#!/usr/bin/pyth0n3
-"""
-Module 2-square
-Defines class Square with private attribute size and validates size
-"""
+#!/usr/bin/python3
+"""Define a class Square."""
 
 
 class Square:
-    """
-    class Square definition
-    Args:
-        size (int): size of a side in square
-    """
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """
-        Initializes square
-        Attributes:
-            __size (int): size of new square, default is 0
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
