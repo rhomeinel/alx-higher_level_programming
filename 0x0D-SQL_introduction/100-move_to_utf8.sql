1443411c094f25916b6affe64700ce26b6c035e6
Rhomeinel <rhomeinel@gmail.com>
2022-08-09 18:28:57 -0700

    Add advanced tasks(17 - 20)

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- -- Converts the entire database hbtn_0c_0 to UTF8.
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- covert table
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
