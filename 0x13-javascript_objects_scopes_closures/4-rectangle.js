8e2678db76269f9e1db8208b2591a4745af83e30
Rhomeinel <rhomeinel@gmail.com>
2022-09-06 16:21:00 -0700

    Modify code

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,10 +1,26 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
+  print () {
+    console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+  }
+
+  rotate () {
+    const h = this.height;
+    const w = this.width;
+    this.width = h;
+    this.height = w;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
   }
 }
-
-module.exports = Square;
+module.exports = Rectangle;
