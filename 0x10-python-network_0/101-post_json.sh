774b74472d0f2ec096593169444f4503e4822737
Rhomeinel <rhomeinel@gmail.com>
2022-09-29 01:05:06 -0700

    Modify script to task

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool" -d "user_id=98"
+# script that sends JSON POST request to URL passed as the first arg
+curl -sH "Content-Type: application/json" -d "$(cat "$2")" "$1"
